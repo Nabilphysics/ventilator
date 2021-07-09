b2f3924aeac9db911793f6c0bef49d3bfc06685d
Syed Razwanul Haque Nabil <nabilsust@gmail.com>
2021-07-10 01:22:53 +0600

    Update vent_SIMV_1.0.ino

--- vent_SIMV_1.0/vent_SIMV_1.0.ino
+++ vent_SIMV_1.0/vent_SIMV_1.0.ino
@@ -12,7 +12,9 @@
   3) Shafi
      UI Software
   4) Hasan
-     3D Design, Assembling      
+     3D Design, Assembling
+  5) Shovon S Saha, shovonsaha3401@gmail.com
+     General Research, Documentation, Scientific Paper
  */  
  /*
   * SIMV Mode works with PRVC mode (ToDo: SIMV with Pressure Control Mode)
@@ -38,7 +40,7 @@ SFM3000CORE sensirionFlow(64); //Sensirion Flow Sensor Object
 // To Do: Replace minimum variable size needed. e.g. uint8_t insteed of int where applicable
 // To Do: Make the code object orienteed. Create Separate Method to Write Motor Speed
 //////////Data Frame Variable//////////
-String getVentFrame;
+String getVentFrame; // Data String Received from Android Tab as a Frame. e.g.  getVentFrame = "v061502.0042090050025150702";
 String mode;
 uint8_t bpm;
 int Tv;
@@ -50,7 +52,7 @@ float peep;
 int pip;
 int breathCycleTime;
 float targetFlowRate;
-
+/// Variables for Alarm
 int TvAlarmHigh;
 int TvAlarmLow;
 int peepAlarmHigh;
@@ -64,35 +66,35 @@ int simvOnOff;
 void setup() {
   Serial.begin(115200);
   Serial.println("Started");
-  Wire.begin();
-  delay(1000); 
-  pinMode(flowSensorOnOff, OUTPUT);
-  digitalWrite(flowSensorOnOff, HIGH);
+  Wire.begin(); // For Flow Sensor
+  delay(1000); // Just for Stability
+  pinMode(flowSensorOnOff, OUTPUT); // 5V Supply for Sensirion SFM3300 Flow Sensor. Since it require maximum 10ma so we are providing power from mcu pin 
+  digitalWrite(flowSensorOnOff, HIGH); // SFM3300 Power Supply Started
   delay(100);
-  sensirionFlow.init();
+  sensirionFlow.init(); // Software Initialization of the SFM3300 Flow Sensor
   delay(1000);
  
-#ifdef dcMotor
+#ifdef dcMotor //Actually Current Turbine from Aliexpress Although its a BLDC motor technically. Previously we used this pin to control a DC motor to provide PWM and write now providing PWM to BLDC Turbine
   pinMode(turbineMotorPin, OUTPUT);
   #endif
 ////////////////// Indicator LED //////////////////////////////
-  pinMode(indicatorLed, OUTPUT);  
+  pinMode(indicatorLed, OUTPUT);  //Green Big LED
 ////////////////// Servo Motor Initialization //////////////////
-  servoOne.attach(servoOnePin);
-  servoTwo.attach(servoTwoPin);
-  oxygenServo.attach(oxygenServoPin);
-  airServo.attach(airServoPin);
-  peepServo.attach(40);
+  servoOne.attach(servoOnePin); // Pressure Release Servo at Pin 5
+  servoTwo.attach(servoTwoPin); // Not implemented
+  oxygenServo.attach(oxygenServoPin);//In this version it has no real effects
+  airServo.attach(airServoPin);//In this version it has no real effects
+  peepServo.attach(40); // In this version it has no real effects
 ///////////////////////////////////////////////////////////////  
-  oxygenServo.write(30);
-  airServo.write(95);
-  sensorCallibration();
-  getVentFrame = "v061502.0042090050025150702";
+  oxygenServo.write(30);//In this version it has no real effects
+  airServo.write(95);//In this version it has no real effects
+  sensorCallibration(); // Pressure Senosr will be callibrated
+  getVentFrame = "v061502.0042090050025150702"; //for testing purpose. In actual use please comment it out
  // getVentFrame = "p105001.5041090050025150702";
  // getVentFrame = "s";
-  mode = getVentFrame.substring(0, 1);
-    bpm = getVentFrame.substring(1, 3).toInt(); //BPM=breath per minute
-    Tv = getVentFrame.substring(3, 6).toInt(); //Tidal Volume in ml
+    mode = getVentFrame.substring(0, 1); //e.g. v or p . v=PRVC mode p=PCV mode 
+    bpm = getVentFrame.substring(1, 3).toInt(); //BPM=breath per minute . Two digit. e.g 06
+    Tv = getVentFrame.substring(3, 6).toInt(); //Tidal Volume in ml , 3 digit. highest 999
     Ti = getVentFrame.substring(6, 9).toFloat() * 1000; //in millisecond
     peep = getVentFrame.substring(9, 11).toInt();
     pip = getVentFrame.substring(11, 13).toInt();
@@ -110,18 +112,18 @@ void setup() {
 //  peep = getVentFrame.substring(9, 11).toFloat();
 //  pip = getVentFrame.substring(11, 13).toInt();
 //  motorSpeed = getVentFrame.substring(13, 16).toInt();
-  breathCycleTime = int((60 / float(bpm)) * 1000); //in millisecond
+  breathCycleTime = int((60 / float(bpm)) * 1000); //in millisecond. e.g. if BPM=10 , breath Cycle Time = 6000ms (Inpiration Time + Expiration Time)
   nonSyncTime = breathCycleTime * 0.9; //for SIMV Mode. to detect Sync Time. e.g. if breath cycle =10 then 9 is non sync period 
-  Te = (breathCycleTime - Ti); //in millisecond
-  syncTime = nonSyncTime - Ti; //(in millisecond)in SIMV if trigger happens in this time period SIMV will give breath early
+  Te = (breathCycleTime - Ti); //in millisecond, Te means Expiration Time during Exhale
+  syncTime = nonSyncTime - Ti; //(in millisecond)in SIMV if trigger happens after this time period of Expiration then SIMV will give breath early
   
-  targetFlowRate = Tv / ((float(Ti) / float(1000))); // in ml
+  targetFlowRate = Tv / ((float(Ti) / float(1000))); // in ml , not needed right now
  
   Serial.println("End of Setup");
 }
 
 void loop() {
-  digitalWrite(indicatorLed, HIGH);
+  digitalWrite(indicatorLed, HIGH); // Green LED ON to indicate that device is operational
   if (Serial.available() > 0) {
 
     getVentFrame = Serial.readString(); 
@@ -153,27 +155,34 @@ void loop() {
 
 /// Restart Mechanism if Sensor Error Occured 
 flowRate = sensirionFlow.getvalue();
-   if((flowRate < -4500.0)||(flowRate > 4500.00)){
+   if((flowRate < -4500.0)||(flowRate > 4500.00)){ // if this condition satisfy that means error.
       Serial.println(flowRate);
-      digitalWrite(flowSensorOnOff, LOW);
+      digitalWrite(flowSensorOnOff, LOW); // Sensor 5V Power OFF
       delay(1000);
-      digitalWrite(flowSensorOnOff, HIGH);
+      digitalWrite(flowSensorOnOff, HIGH); // Sensor 5V Power ON, for Hard Reset 
       delay(500);
       sensirionFlow.init();
       delay(50);
-      sensirionFlow.softReset();
+      sensirionFlow.softReset(); //Software Reset
       sensirionFlow.init();
       delay(1000);
-      motorSpeed = 30;
+      motorSpeed = 30; //Just to Run Slowly
+     //To Do = Create Flow Sensor Error Alarm
     }
 
   ///Volume Control Mode//////
   if (mode == "v") {
    
-    volumeControl();
+    volumeControl(); //PRVC
     //inspiratoryLoop();
   }
-  if (mode == "s"){
+  
+  if (mode == "p") { //PCV = Pressure Control Ventilation
+    //stabilizingCounter = 0;
+    pressureControl();
+  }
+  
+  if (mode == "s"){ //Debugging Mode. Its not actually a mode. Its for Testing. It will just execute below procedure. 
     analogWrite(turbineMotorPin,0);
     
     //servoControl("close");
@@ -224,10 +233,7 @@ flowRate = sensirionFlow.getvalue();
 //Serial.println(highestPressureCalculator(testData, sizeof(testData)/sizeof(float)));
   }
  
-  if (mode == "p") {
-    //stabilizingCounter = 0;
-    pressureControl();
-  }
+  
   
 
 }
